d8f659991608b3aae1f95150efb9f7cb787c1b54
Deydre <deydrealonso@gmail.com>
2024-10-10 21:43:16 +0200

    add/ script.js

--- script.js
+++ script.js
@@ -1,248 +1,192 @@
-let firebaseConfig = { 
-    apiKey: "AIzaSyDUXRP7fah5PnvoUV02W7BsfKjqOJLh7oM",
-    authDomain: "demoweb-46446.firebaseapp.com",
-    projectId: "demoweb-46446",
-    storageBucket: "demoweb-46446.appspot.com",
-    messagingSenderId: "460314176652",
-    appId: "1:460314176652:web:691c74421911536a0f9fa9"
-  };
-  
-  firebase.initializeApp(firebaseConfig);// Inicializaar app Firebase
-  
-  const db = firebase.firestore();// db representa mi BBDD //inicia Firestore
-  
-  //Función auxiliar para pintar una foto en el album
-  const printPhoto = (title, url, docId) => {
-    let card = document.createElement('article');
-    card.setAttribute('class', 'card');
-    let picture = document.createElement('img');
-    picture.setAttribute('src', url);
-    picture.setAttribute('style', 'max-width:250px');
-    let caption = document.createElement('p');
-    caption.innerHTML = title;
-    let id = document.createElement('p');
-    id.innerHTML = docId;
-    const album = document.getElementById('album');
-    card.appendChild(picture);
-    card.appendChild(caption);
-    card.appendChild(id);
-    album.appendChild(card);
-  };
-  
-  //Create
-  const createPicture = (picture) => {
-    db.collection("album")
-      .add(picture)
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id)
-        readAll();
-      })
-      .catch((error) => console.error("Error adding document: ", error));
-  };
-  
-  //Read all
-  const readAll = () => {
-    // Limpia el album para mostrar el resultado
-    cleanAlbum();
-  
-    //Petición a Firestore para leer todos los documentos de la colección album
-    db.collection("album")
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          printPhoto(doc.data().title, doc.data().url, doc.id)
-        });
-  
-      })
-      .catch(() => console.log('Error reading documents'));;
-  };
-  
-  //Delete
-  const deletePicture = () => {
-    const id = prompt('Introduce el ID a borrar');
-    db.collection('album').doc(id).delete().then(() => {
-      alert(`Documento ${id} ha sido borrado`);
-      //Clean
-      document.getElementById('album').innerHTML = "";
-      //Read all again
-      readAll();
+// Credenciales de la BBDD
+const firebaseConfig = {
+  apiKey: "AIzaSyCjgqh_mQ0oduPSq_tqgUCK3NNzhpX6WHo",
+  authDomain: "demoweb-dfb33.firebaseapp.com",
+  projectId: "demoweb-dfb33",
+  storageBucket: "demoweb-dfb33.appspot.com",
+  messagingSenderId: "946005046680",
+  appId: "1:946005046680:web:027a4111122fd14aeee7ed"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);//
+
+// db representa mi BBDD //inicia FirestoreS
+const db = firebase.firestore();
+
+// ---- FUNCIONES AUXILIARES -----------------------------
+// Crear user en la BBDD
+const createUser = (user) => {
+  db.collection("users")
+    .add(user)
+    .then((docRef) => console.log("Elemento escrito en BBDD. ID:", docRef.id))
+    .catch((error) => console.error("Error adding document: ", error));
+};
+
+// Borrar lista DOM
+const cleanContactList = () => {
+  document.getElementById('contactList').innerHTML = "";
+};
+
+
+// ---- FUNCIONES --------------------------------------
+// Pintar datos en el DOM
+const printData = (nombre, email, mensaje, urlImagen) => {
+  let card = document.createElement('article');
+  card.setAttribute('class', 'card');
+
+  let nombreContainer = document.createElement('h3');
+  nombreContainer.innerHTML = nombre;
+
+  let imgContainer = document.createElement('img');
+  imgContainer.setAttribute('src', urlImagen);
+  imgContainer.setAttribute('style', 'max-width:250px');
+
+  let emailContainer = document.createElement('p');
+  emailContainer.innerHTML = email;
+
+  let mensajeContainer = document.createElement('p');
+  mensajeContainer.innerHTML = mensaje;
+
+  card.appendChild(nombreContainer);
+  card.appendChild(imgContainer);
+  card.appendChild(emailContainer);
+  card.appendChild(mensajeContainer);
+  document.getElementById('contactList').appendChild(card);
+};
+
+
+// Borrar lista DOM, leer los datos de la BBDD y pintarlos
+const readAndPrintAll = () => {
+  // Borrar lista DOM
+  cleanContactList();
+
+  // Leer datos de la BBDD
+  db.collection("users")
+    .get()
+    .then((querySnapshot) => {
+      console.log('Documents fetched:', querySnapshot.size);
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        // Pintar todos con la función printData()
+        printData(data.nombre, data.email, data.mensaje, data.urlImagen)
+      });
     })
-      .catch(() => console.log('Error borrando documento'));
-  };
+    .catch((e) => console.log('Error reading documents:', e.code));
+};
+
+// VER CÓMO USAR PARA BORRAR TODO ----------------
+//Delete
+const deletePicture = () => {
+  const id = prompt('Introduce el ID a borrar');
+  db.collection('users').doc(id).delete().then(() => {
+    alert(`Documento ${id} ha sido borrado`);
+    //Clean
+    document.getElementById('contactList').innerHTML = "";
+    //Read all again
+    readAndPrintAll();
+  })
+    .catch(() => console.log('Error borrando documento'));
+};
+
+// Función para borrar todos los documentos de la colección "users"
+const deleteAllContacts = () => {
+
+  // Pedir confirmación
+  const confirmation = confirm("¿Estás seguro de que deseas borrar todos los contactos?");
   
-  //Clean 
-  const cleanAlbum = () => {
-    document.getElementById('album').innerHTML = "";
-  };
-  
-  //Show on page load
-  /* readAll(); */
-  
-  //**********EVENTS**********
-  
-  //Create
-  document.getElementById("create").addEventListener("click", () => {
-    const title = prompt("Introduce el título de tu foto");
-    const url = prompt("Introduce la url de tu foto");
-    if (!title || !url) {
-      alert("Hay un campo vacio. No se ha salvado");
-      return
-    }
-    createPicture({
-      title,
-      url,
-    });
-  });
-  
-  //Read all
-  document.getElementById("read-all").addEventListener("click", () => {
-    readAll();
-  });
-  
-  //Read one
-  document.getElementById('read-one').addEventListener("click", () => {
-    const id = prompt("Introduce el id a buscar");
-    readOne(id);
-  });
-  
-  //Delete one
-  document.getElementById('delete').addEventListener('click', () => {
-    deletePicture();
-  });
-  
-  //Clean
-  document.getElementById('clean').addEventListener('click', () => {
-    cleanAlbum();
-  });
-  
-  //********FIRESTORE USERS COLLECTION******
-  
-  const createUser = (user) => {
-    db.collection("users")
-      .add(user)
-      .then((docRef) => console.log("Document written with ID: ", docRef.id))
-      .catch((error) => console.error("Error adding document: ", error));
-  };
-  
-  /* const readAllUsers = (born) => {
-    db.collection("users")
-      .where("first", "==", born)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          console.log(doc.data());
+  if (confirmation) {
+    // Obtenemos la referencia a la colección "users" y hacemos la consulta para obtener todos los documentos
+    db.collection("users").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // Eliminamos cada documento individualmente
+        db.collection('users').doc(doc.id).delete().then(() => {
+          console.log(`Documento ${doc.id} ha sido borrado.`);
+        }).catch((error) => {
+          console.error(`Error borrando el documento con ID ${doc.id}:`, error);
         });
       });
-  }; */
-  
-  // Read ONE
-  function readOne(id) {
-    // Limpia el album para mostrar el resultado
-    cleanAlbum();
-  
-    //Petición a Firestore para leer un documento de la colección album 
-    var docRef = db.collection("album").doc(id);
-  
-    docRef.get().then((doc) => {
-      if (doc.exists) {
-        console.log("Document data:", doc.data());
-        printPhoto(doc.data().title, doc.data().url, doc.id);
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
+
+      // Limpia la lista de contactos
+      document.getElementById('contactList').innerHTML = "";
+      
+      // Vuelve a cargar los contactos restantes (vacío si se han borrado todos)
+      readAndPrintAll();
+
     }).catch((error) => {
-      console.log("Error getting document:", error);
-    });
-  
-  }
-  
-  /**************Firebase Auth*****************/
-  
-  /*
-  
-  const signUpUser = (email, password) => {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Signed in
-        let user = userCredential.user;
-        console.log(`se ha registrado ${user.email} ID:${user.uid}`)
-        alert(`se ha registrado ${user.email} ID:${user.uid}`)
-        // ...
-        // Saves user in firestore
-        createUser({
-          id: user.uid,
-          email: user.email,
-          message: "hola!"
-        });
-  
-      })
-      .catch((error) => {
-        console.log("Error en el sistema" + error.message, "Error: " + error.code);
-      });
-  };
-  
-  
-  document.getElementById("form1").addEventListener("submit", function (event) {
-    event.preventDefault();
-    let email = event.target.elements.email.value;
-    let pass = event.target.elements.pass.value;
-    let pass2 = event.target.elements.pass2.value;
-  
-    pass === pass2 ? signUpUser(email, pass) : alert("error password");
-  })
-  
-  
-  const signInUser = (email, password) => {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Signed in
-        let user = userCredential.user;
-        console.log(`se ha logado ${user.email} ID:${user.uid}`)
-        alert(`se ha logado ${user.email} ID:${user.uid}`)
-        console.log("USER", user);
-      })
-      .catch((error) => {
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        console.log(errorCode)
-        console.log(errorMessage)
-      });
-  }
-  
-  const signOut = () => {
-    let user = firebase.auth().currentUser;
-  
-    firebase.auth().signOut().then(() => {
-      console.log("Sale del sistema: " + user.email)
-    }).catch((error) => {
-      console.log("hubo un error: " + error);
+      console.error("Error obteniendo los contactos:", error);
     });
   }
+};
+
+// Asignar función al evento de hacer click en BORRAR TODOS
+document.getElementById("clearAll").addEventListener("click", deleteAllContacts);
+
+
+
+// VER CÓMO USAR PARA BORRAR SOLO 1 ----------------
+// // Read ONE
+// function readOne(id) {
+//   // Limpia el album para mostrar el resultado
+//   cleanContactList();
+
+//   //Petición a Firestore para leer un documento de la colección album 
+//   var docRef = db.collection("users").doc(id);
+
+//   docRef.get().then((doc) => {
+//     if (doc.exists) {
+//       console.log("Document data:", doc.data());
+//       let data = doc.data();
+//       printData(data.nombre, data.email, data.mensaje, data.urlImagen);
+//     } else {
+//       // doc.data() will be undefined in this case
+//       console.log("No such document!");
+//     }
+//   }).catch((error) => {
+//     console.log("Error getting document:", error.code);
+//   });
+
+// }
+
+// ----------------------------
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // obtener referencias a los elementos en HTML
+  let formularioDeContacto = document.getElementById("contactForm");
+  // let listaDeContactos = document.getElementById("contactList");
+  let botonBorrarTodos = document.getElementById("clearAll");
   
-  
-  document.getElementById("form2").addEventListener("submit", function (event) {
-    event.preventDefault();
-    let email = event.target.elements.email2.value;
-    let pass = event.target.elements.pass3.value;
-    signInUser(email, pass)
-  })
-  document.getElementById("salir").addEventListener("click", signOut);
-  
-  // Listener de usuario en el sistema
-  // Controlar usuario logado
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      console.log(`Está en el sistema:${user.email} ${user.uid}`);
-      document.getElementById("message").innerText = `Está en el sistema: ${user.uid}`;
-    } else {
-      console.log("no hay usuarios en el sistema");
-      document.getElementById("message").innerText = `No hay usuarios en el sistema`;
-    }
+  // Leer los datos y pintarlos
+  readAndPrintAll(); 
+
+  // cuando el formulario se envía
+  formularioDeContacto.addEventListener("submit", function (evento) {
+    evento.preventDefault(); // evitar que la página se recargue
+
+    // obtener los valores que el usuario escribió
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let mensaje = document.getElementById("mensaje").value;
+    let urlImagen = document.getElementById("imageUrl").value;
+
+    // Crear en el DOM
+    createUser({nombre, email, mensaje, urlImagen});
+
+    formularioDeContacto.reset(); // reiniciar el formulario
+    readAndPrintAll(); // actualizar la lista
   });
-  
-  */
-  
-  
+
+  // Evento para borrar todos los contactos
+  botonBorrarTodos.addEventListener("click", function () {
+    db.collection("users").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        doc.ref.delete(); // Borrar cada documento
+      });
+    }).then(() => {
+      alert("Todos los contactos han sido borrados.");
+      readAndPrintAll(); // Leer todos los contactos después de borrar todos
+    });
+  });
+
+});
